c804caf6addf1737b72528d6e7512ac46c12475b
Promise <promiseezema11@gmail.com>
2022-10-27 15:10:29 +0100

    Heroku Procfile

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,9 +26,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Route::pattern('domain', '[a-z0-9.\-]+'); 
-        // parent::boot();
-
         $this->configureRateLimiting();
 
         $this->routes(function () {
