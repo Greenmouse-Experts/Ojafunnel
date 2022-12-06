d1e1ff2cf459d7ed10a0da772811eccb6b846746
Promise <promiseezema11@gmail.com>
2022-12-06 11:08:48 +0100

    updated

--- resources/views/dashboard/pageBuilder.blade.php
+++ resources/views/dashboard/pageBuilder.blade.php
@@ -74,9 +74,6 @@
                                 @endif
                                 <div class="list">
                                     <ul class="list-unstyled hstack gap-3 mb-0">
-                                        <li title="Contact">
-                                            <a data-bs-toggle="modal" data-bs-target="#forming" class="btn btn-sm btn-soft-success"><i class="bi bi-person-circle"></i></a>
-                                        </li>
                                         <li title="Edit">
                                             <a data-bs-toggle="modal" data-bs-target="#Editing-{{$page->id}}" class="btn btn-sm btn-soft-info"><i class="mdi mdi-pencil-outline"></i></a>
                                         </li>
@@ -199,65 +196,4 @@
     <!-- End Page-content -->
 </div>
 <!-- END layout-wrapper -->
-
-<!-- forming modal -->
-<div class="modal fade" id="forming" tabindex="-1" aria-labelledby="subscribeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-0">
-                <h5 class="modal-title" id="staticBackdropLabel">
-                    Fill all information below
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="Edit-level">
-                        <form>
-                            <div class="form">
-                                <div class="col-lg-12">
-                                    <label>First Name</label>
-                                    <div class="row">
-                                        <div class="col-md-12 mb-4">
-                                            <input type="text" placeholder="Your First Name" name="name" class="input" required>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12">
-                                    <label>Last Name</label>
-                                    <div class="row">
-                                        <div class="col-md-12 mb-4">
-                                            <input type="text" placeholder="Your Last Name" name="name" class="input" required>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12">
-                                    <label>Email</label>
-                                    <div class="row">
-                                        <div class="col-md-12 mb-4">
-                                            <input type="email" placeholder="Your Email" name="email" class="input" required>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row justify-content-between">
-                                    <div class="col-6">
-                                        <button data-bs-dismiss="modal" aria-label="Close" class="btn px-3" style="color: #714091; border: 1px solid #714091">
-                                            Cancel
-                                        </button>
-                                    </div>
-                                    <div class="col-6 text-end">
-                                        <button class="form-btn btn px-4" type="submit" style="color: #ffffff; background-color: #714091">
-                                            Save
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!--forming end modal -->
 @endsection
