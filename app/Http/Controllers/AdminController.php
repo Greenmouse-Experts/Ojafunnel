5ab71cbd65dacd99cd58fb2ab633f81b4e1137af
Hamzat <hmztadeleke@gmail.com>
2022-12-09 11:54:13 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,15 +10,20 @@ use Tzsk\Sms\Facades\Sms;
 use GuzzleHttp\Client;
 
 class AdminController extends Controller
-{
-    //
-    public function adminwelcome()
     {
-        return view('admin.adminwelcome');
-    }
+        //
+        public function adminwelcome()
+        {
+            return view('admin.adminwelcome');
+        }
 
-    public function viewlist()
-    {
-        return view('admin.viewlist');
-    }
+        public function viewlist()
+        {
+            return view('admin.viewlist');
+        }
+
+        public function viewmessage()
+        {
+            return view('admin.viewmessages');
+        }
 }
