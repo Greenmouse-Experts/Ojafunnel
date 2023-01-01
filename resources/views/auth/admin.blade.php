3fc7561ce9796d7d7ea92c1e7086da7087570fb5
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2023-01-01 21:46:00 +0100

    Update

--- resources/views/auth/admin.blade.php
+++ resources/views/auth/admin.blade.php
@@ -83,13 +83,7 @@
                             </div>
                         </div>
                         <div class="col-lg-4">
-                            <!-- <div class="btn-divv">
-                                <div class="portlog">
-                                    <a href="/">
-                                        <img src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1662630837/OjaFunnel-Images/signup-image_ecquqt.png" draggable="false">
-                                    </a>
-                                </div>
-                            </div> -->
+                           
                         </div>
                     </div>
                 </div>
