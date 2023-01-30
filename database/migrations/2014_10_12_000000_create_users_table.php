643823478bf3dccb586b03ef1880ffdfd2ac7abe
Moshood Gbadamosi <morshud@users.noreply.github.com>
2023-01-30 13:30:26 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,24 +14,27 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->comment('');
+            $table->bigIncrements('id');
             $table->string('user_type')->nullable();
             $table->string('affiliate_link')->nullable();
             $table->string('code')->nullable();
             $table->string('photo')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('username', 100)->unique()->nullable();
+            $table->string('username', 100)->nullable()->unique();
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
             $table->string('referral_link')->nullable();
+            $table->string('ref_bonus')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('plan')->nullable();
-            $table->string('ref_bonus')->default(0);
+            $table->unsignedInteger('customer_id')->nullable()->index('mailusers_customer_id_foreign');
             $table->enum('status', ['active', 'inactive'])->default('active')->index();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('wallet')->default('0.00');
         });
     }
 
