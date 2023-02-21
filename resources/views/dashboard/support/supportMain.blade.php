d9b7692ba17a4952cef3240dc73f9e8c7c0ee7da
Hamzat <hmztadeleke@gmail.com>
2023-02-21 20:10:40 +0100

    support update

--- resources/views/dashboard/support/supportMain.blade.php
+++ resources/views/dashboard/support/supportMain.blade.php
@@ -45,9 +45,9 @@
                         <img src='https://res.cloudinary.com/greenmouse-tech/image/upload/v1676990796/OjaFunnel-Images/chat-clip-removebg-preview_weurrv.png' alt='live-chat' />
                         <a href='' class="text-center">Live Chat</a>
                     </div>
-                    <div class='col-md-4 support-option'>
+                    <div class='col-md-4 support-option' data-bs-toggle="modal" data-bs-target="#sendMail">
                         <img src='https://res.cloudinary.com/greenmouse-tech/image/upload/v1676991710/OjaFunnel-Images/email-chat-removebg-preview_o6kgo7.png' alt='live-chat' />
-                        <a href="" class="text-center">Send Mail</a>
+                        <a href="javascript: void(0);" class="text-center">Send Mail</a>
                     </div>
                 </div>
             </div>
@@ -55,5 +55,61 @@
     </div>
     <!-- End Page-content -->
 </div>
+<div class="modal fade" id="sendMail" tabindex="-1" aria-labelledby="subscribeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        Send Instant Mail to Ojafunnel
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="Edit-level">
+                            <form  method="post">
+                                <div class="form">
+                                    <div class="col-lg-12">
+                                        <label>Email</label>
+                                        <div class="row">
+                                            <div class="col-md-12 mb-4">
+                                                <input type="text" placeholder="Enter your email..." name="name" class="input"
+                                                    required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <label>Your Message</label>
+                                        <div class="row">
+                                            <div class="col-md-12 mb-4">
+                                                <textarea></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row justify-content-between">
+                                        <div class="col-6">
+                                            <a href="#" class="text-decoration-none">
+                                                <button class="btn px-3" style="color: #714091; border: 1px solid #714091">
+                                                    Cancel
+                                                </button></a>
+                                        </div>
+                                        <div class="col-6 text-end">
+                                            <a href="#" class="text-decoration-none">
+                                                <button type="submit" class="btn px-4" style="color: #ffffff; background-color: #714091"
+                                                    >
+                                                    Save
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- END layout-wrapper -->
 @endsection
