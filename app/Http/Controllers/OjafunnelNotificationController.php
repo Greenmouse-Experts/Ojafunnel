d4d543dc94650fae00eddb41cf43fc264a01114d
Promise <promiseezema11@gmail.com>
2023-02-24 16:08:33 +0100

    updated

--- app/Http/Controllers/OjafunnelNotificationController.php
+++ app/Http/Controllers/OjafunnelNotificationController.php
@@ -20,7 +20,7 @@ class OjafunnelNotificationController extends Controller
             'message' => ['required', 'string'],
         ]);
 
-        $admin = Admin::latest()->where('name', 'Administrator')->first();
+        $admin = Admin::latest()->where('name', 'Admin')->first();
 
         OjafunnelMailSupport::create([
             'user_id' => Auth::user()->id,
