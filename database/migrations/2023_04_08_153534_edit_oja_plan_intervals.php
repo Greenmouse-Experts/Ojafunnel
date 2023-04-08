06106052c5ece91bff47897208340fbfeb3a9dd4
Promise <promiseezema11@gmail.com>
2023-04-08 15:37:58 +0100

    updated

---
+++ database/migrations/2023_04_08_153534_edit_oja_plan_intervals.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oja_plan_intervals', function (Blueprint $table) {
+            $table->string('currency_sign')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
