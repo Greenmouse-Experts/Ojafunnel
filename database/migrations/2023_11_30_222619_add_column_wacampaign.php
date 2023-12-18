798ee45dd9fbceb7179699fdb11453e9cab6eff7
cleanprogrammer2026 <cleanprogrammer2026@gmail.com>
2023-12-18 10:56:27 +0100

    fixing series

--- database/migrations/2023_11_30_222619_add_column_wacampaign.php
+++ database/migrations/2023_11_30_222619_add_column_wacampaign.php
@@ -13,9 +13,32 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('wa_campaigns', function (Blueprint $table) {
+        // Auxilliary table for whstapp capaigns.
+        // Schema::table('wa_campaigns_listener', function (Blueprint $table) {
 
-            $table->boolean('notify_every_newcontact');
+        //     $table->boolean('notify_every_newcontact');
+        // });
+
+        // Auxilliary table for whstapp capaigns.
+        Schema::table('sms_campaigns_listener', function (Blueprint $table) {
+
+            // Contain the list sequence of the scheduled messages in the campaign
+            // list ID
+            // campaign ID
+        });
+
+        // Auxilliary table for whstapp capaigns.
+        Schema::table('email_campaigns_listener', function (Blueprint $table) {
+            // Contain the list sequence of the scheduled messages in the campaign
+            // list ID
+            // campaign ID
+        });
+
+        // Auxilliary table for whstapp capaigns.
+        Schema::table('whatsapp_campaigns_listener', function (Blueprint $table) {
+            // Contain the list sequence of the scheduled messages in the campaign
+            // list ID
+            // campaign ID
         });
     }
 
