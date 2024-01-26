fbf7a3636d5c9d52958a8f181831c2d1cef15a0f
cleanprogrammer2026 <cleanprogrammer2026@gmail.com>
2024-01-26 23:10:46 +0100

    fixed second phase of the wa automation

--- app/Http/Controllers/ListManagementController.php
+++ app/Http/Controllers/ListManagementController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\ListManagement;
 use App\Models\ListManagementContact;
+use App\Models\WaCampaigns;
+use App\Models\SeriesWaCampaign;
+use App\Models\CandidateWASeries;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -272,7 +276,7 @@ class ListManagementController extends Controller
         //     ]);
         // }
 
-        ListManagementContact::create([
+        $listManagementContact = ListManagementContact::create([
             'uid' => Str::uuid(),
             'list_management_id' => $list->id,
             'name' => $request->name,
@@ -288,6 +292,66 @@ class ListManagementController extends Controller
             'subscribe' => true,
             'tags' => $request->tags,
         ]);
+
+        // check if list_mgt_id is tied to any wa campaign;
+        $campaign = WaCampaigns::where('contact_list_id', $listManagementContact->list_management_id)
+            ->orderBy('id', 'DESC') // Filter the last campaign ran
+            ->first();
+
+        if(!is_null($campaign)) {
+
+            // with the following detail @ hand; you can proceed to restart a fresh series campaign
+            // for the newly added candidate.
+            $wa_series = SeriesWaCampaign::where(['wa_campaign_id' => $campaign->id])->get();
+
+            $old_dates = SeriesWaCampaign::where(['wa_campaign_id' => $campaign->id])->pluck('date')->toArray();
+            $old_times = SeriesWaCampaign::where(['wa_campaign_id' => $campaign->id])->pluck('time')->toArray();
+            $last_created_series = null;
+
+            $index = 0;
+
+            foreach ($wa_series as $item) 
+            {
+                $now = Carbon::now();
+
+                if($index == 0) 
+                {
+                    // add 1hr to kick time
+                    $date = Carbon::parse($now)->addHour();
+                    $last_created_series = CandidateWASeries::create([
+                        'user_id' => $item->user_id,
+                        'wa_campaign_id' => $campaign->id,
+                        'date' => $date->format('Y-m-d'),
+                        'time' => $date->format('h:i:s'),
+                        'message' => $item->message,
+                        'contact_count' => 1,
+                        'delivered_count' => 0,
+                        'status' => 'Waiting'
+                    ]);
+                } else {
+                    $last_date = $last_created_series->date;
+                    $last_old_date = new Carbon($old_dates[$index - 1]);
+                    $curr_old_date = new Carbon($old_dates[$index]);
+
+                    $getintvl = $last_old_date->diffInDays($curr_old_date);
+                    $new_date = Carbon::parse($last_date)->addDays($getintvl)->format('Y-m-d');
+
+                    $last_created_series = CandidateWASeries::create([
+                        'user_id' => $item->user_id,
+                        'wa_campaign_id' => $campaign->id,
+                        'date' => $new_date,
+                        'time' => $last_created_series->time,
+                        'message' => $item->message,
+                        'contact_count' => 1,
+                        'delivered_count' => 0,
+                        'status' => 'Waiting'
+                    ]);
+                }
+                
+
+                $index++;
+            }
+        }
 
         return redirect()->route('user.view.list', Crypt::encrypt($list->id))->with([
             'type' => 'success',
