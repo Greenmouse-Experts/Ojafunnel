33ad6eb0cb3dfd879ead4fb928f706dbc3a5bc9c
Promise <promiseezema11@gmail.com>
2024-02-08 10:38:19 +0100

    updated

--- app/Console/Commands/EmailBirthday.php
+++ app/Console/Commands/EmailBirthday.php
@@ -43,72 +43,72 @@ class EmailBirthday extends Command
             'action' => 'Play'
         ])->whereDate('start_date', '<=', $current_date)->whereDate('end_date', '>=', $current_date)->get();
 
-        Log::info($birthday_automation);
+        Log::info($current_date);
 
-        $birthday_automation->map(function ($_campaign) use ($date) {
-            $email_kit = EmailKit::where('id', $_campaign->email_kit_id)->first();
+        // $birthday_automation->map(function ($_campaign) use ($date) {
+        //     $email_kit = EmailKit::where('id', $_campaign->email_kit_id)->first();
 
-            $current_day = $date->format('d');
-            $current_month = $date->format('m');
+        //     $current_day = $date->format('d');
+        //     $current_month = $date->format('m');
 
-            if ($_campaign->sms_type == 'birthday') {
+        //     if ($_campaign->sms_type == 'birthday') {
 
-                $lists = ListManagementContact::where(['list_management_id' => $_campaign->birthday_contact_list_id])
-                    ->whereMonth('date_of_birth', $current_month)
-                    ->whereDay('date_of_birth', $current_day)->get();
+        //         $lists = ListManagementContact::where(['list_management_id' => $_campaign->birthday_contact_list_id])
+        //             ->whereMonth('date_of_birth', $current_month)
+        //             ->whereDay('date_of_birth', $current_day)->get();
 
-                Log::info($lists);
-                // divide into 500 chunks and
-                // delay each job between 10  - 20 sec in the queue
-                $chunks = $lists->chunk(500);
-                $delay = mt_rand(10, 20);
+        //         Log::info($lists);
+        //         // divide into 500 chunks and
+        //         // delay each job between 10  - 20 sec in the queue
+        //         $chunks = $lists->chunk(500);
+        //         $delay = mt_rand(10, 20);
 
-                // template 1
-                // dispatch job and delay
-                foreach ($chunks as $key => $_chunk) {
-                    // dispatch job
-                    ProcessEmailBirthday::dispatch([
-                        'smtp_host'    => $email_kit->host,
-                        'smtp_port'    => $email_kit->port,
-                        'smtp_username'  => $email_kit->username,
-                        'smtp_password'  => $email_kit->password,
-                        'from_email'    => $email_kit->from_email,
-                        'from_name'    => $email_kit->from_name,
-                    ], $_chunk, ['email_kit' => $email_kit, 'birthday_automation' => $_campaign])
-                        ->onQueue('emailBirthday')->delay($delay);
+        //         // template 1
+        //         // dispatch job and delay
+        //         foreach ($chunks as $key => $_chunk) {
+        //             // dispatch job
+        //             ProcessEmailBirthday::dispatch([
+        //                 'smtp_host'    => $email_kit->host,
+        //                 'smtp_port'    => $email_kit->port,
+        //                 'smtp_username'  => $email_kit->username,
+        //                 'smtp_password'  => $email_kit->password,
+        //                 'from_email'    => $email_kit->from_email,
+        //                 'from_name'    => $email_kit->from_name,
+        //             ], $_chunk, ['email_kit' => $email_kit, 'birthday_automation' => $_campaign])
+        //                 ->onQueue('emailBirthday')->delay($delay);
 
-                    $delay += mt_rand(10, 20);
-                }
-            }
+        //             $delay += mt_rand(10, 20);
+        //         }
+        //     }
 
-            if ($_campaign->sms_type == 'anniversary') {
-                $lists = ListManagementContact::where(['list_management_id' => $_campaign->birthday_contact_list_id])
-                    ->whereMonth('anniv_date', $current_month)
-                    ->whereDay('anniv_date', $current_day)->get();
+        //     if ($_campaign->sms_type == 'anniversary') {
+        //         $lists = ListManagementContact::where(['list_management_id' => $_campaign->birthday_contact_list_id])
+        //             ->whereMonth('anniv_date', $current_month)
+        //             ->whereDay('anniv_date', $current_day)->get();
 
-                // divide into 500 chunks and
-                // delay each job between 10  - 20 sec in the queue
-                $chunks = $lists->chunk(500);
-                $delay = mt_rand(10, 20);
+        //         // divide into 500 chunks and
+        //         // delay each job between 10  - 20 sec in the queue
+        //         $chunks = $lists->chunk(500);
+        //         $delay = mt_rand(10, 20);
 
-                // template 1
-                // dispatch job and delay
-                foreach ($chunks as $key => $_chunk) {
-                    // dispatch job
-                    ProcessEmailBirthday::dispatch([
-                        'smtp_host'    => $email_kit->host,
-                        'smtp_port'    => $email_kit->port,
-                        'smtp_username'  => $email_kit->username,
-                        'smtp_password'  => $email_kit->password,
-                        'from_email'    => $email_kit->from_email,
-                        'from_name'    => $email_kit->from_name,
-                    ], $_chunk, ['email_kit' => $email_kit, 'birthday_automation' => $_campaign])
-                        ->onQueue('emailBirthday')->delay($delay);
+        //         // template 1
+        //         // dispatch job and delay
+        //         foreach ($chunks as $key => $_chunk) {
+        //             // dispatch job
+        //             ProcessEmailBirthday::dispatch([
+        //                 'smtp_host'    => $email_kit->host,
+        //                 'smtp_port'    => $email_kit->port,
+        //                 'smtp_username'  => $email_kit->username,
+        //                 'smtp_password'  => $email_kit->password,
+        //                 'from_email'    => $email_kit->from_email,
+        //                 'from_name'    => $email_kit->from_name,
+        //             ], $_chunk, ['email_kit' => $email_kit, 'birthday_automation' => $_campaign])
+        //                 ->onQueue('emailBirthday')->delay($delay);
 
-                    $delay += mt_rand(10, 20);
-                }
-            }
-        });
+        //             $delay += mt_rand(10, 20);
+        //         }
+        //     }
+        // });
 
         return Command::SUCCESS;
     }
