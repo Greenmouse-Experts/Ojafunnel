675b2416d84818ba99a5db39ffd9884a0d46d9fd
Promise <promiseezema11@gmail.com>
2024-04-11 15:21:13 +0100

    updated

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -31,7 +31,10 @@ class Cors
         ];
 
         if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+            return response('', 200)
+                    ->header('Access-Control-Allow-Origin', '*')
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Token-Auth, X-Requested-With');
         }
 
         $response = $next($request);
