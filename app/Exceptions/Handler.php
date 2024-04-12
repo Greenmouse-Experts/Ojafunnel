06d1d521941f0bc32036ee98e23c5ebe31dcf147
Promise <promiseezema11@gmail.com>
2024-04-12 11:38:09 +0100

    updated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,15 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use BadMethodCallException;
+use ErrorException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,6 +54,55 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' =>  str_contains($e->getMessage(), 'The route') ? 'Endpoint not found. If error persists, contact '.config('app.name').' customer care.' : (str_contains($e->getMessage(), 'No query results') ? str_replace(']', '', last(explode('\\', $e->getMessage()))) . ' not found.' : $e->getMessage())
+                ]);
+            }
+        });
+        $this->renderable(function (ServiceUnavailableHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' => 'Server Error. If error persists, contact '.config('app.name').' customer care.'
+                ], 500);
+            }
+        });
+        $this->renderable(function (BadRequestHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' => 'Invalid request'
+                ]);
+            }
+        });
+        $this->renderable(function (ErrorException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' => 'Failed to get service'
+                ]);
+            }
+        });
+        $this->renderable(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' => 'The method is not supported for this route.'
+                ]);
+            }
+        });
+        $this->renderable(function (BadMethodCallException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response([
+                    'status' => false,
+                    'message' => 'Invalid request. If error persists, contact '.config('app.name').' customer care.'
+                ]);
+            }
         });
     }
 }
